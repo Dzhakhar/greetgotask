cfa48f7c141891a74dbd777b8ed0923c10b685aa
dzhakhar <dzhakhar.ukhaev@gmail.com>
2016-07-27 07:17:14 +0600

    DB autorun

---
+++ src/main/java/hello/Rest.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class Rest {
+
+    private final long id;
+    private final String content;
+
+    public Rest(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/main/sql/schema.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE school;
+\c school;
+CREATE TABLE students(id SERIAL PRIMARY KEY, name varchar(20), surname varchar(25));
